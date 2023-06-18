8cf4ea1dfbe7cabd4919a8328b764358fd82bb19
OzielUnidesc <oziel.junior@sounidesc.com.br>
2023-06-18 16:23:32 -0300

    Alterando a Firstscreen

--- flutter_ddm/lib/Firstscreen.dart
+++ flutter_ddm/lib/Firstscreen.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
-    
-class Firstscreen extends StatelessWidget {
 
-  const Firstscreen({ Key? key }) : super(key: key);
-  
+class Firstscreen extends StatelessWidget {
+  const Firstscreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
-      ),
-      body: Container(),
+      appBar: AppBar(title: const Text("Primeira tela")),
+      body: Container(
+          color: Colors.amber,
+          child: Center(
+              child: ElevatedButton(
+            style: ElevatedButton.styleFrom(padding: const EdgeInsets.all(20)),
+            onPressed: () {},
+            child: const Text("Ir para segunda tela"),
+          ))),
     );
   }
 }
