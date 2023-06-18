44531dff33548cd29b0981fce0711b45220f88cd
Larissa Cândida dos Santos <Larissa.candida@sounidesc.com.br>
2023-06-18 16:00:17 -0300

    Firstscreen incompleta

---
+++ flutter_ddm/lib/Firstscreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+    
+class Firstscreen extends StatelessWidget {
+
+  const Firstscreen({ Key? key }) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
